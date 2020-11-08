39f8d93ea5cf6a41e7e3c1b6cfd654ec496cce3e
Astroner <zhenyanikforov@gmail.com>
2020-11-08 04:19:39 +0300

    envs for port & host

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
+import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(env.DAEMON_PORT, env.DAEMON_HOST, async () => {
+    console.log(`Started on ${await app.getUrl()}`);
+  });
 }
 bootstrap();
