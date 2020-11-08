43204a39de35792db1a05a50a8c7198b42977751
Astroner <zhenyanikforov@gmail.com>
2020-11-08 04:30:27 +0300

    new env

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/mmo_app',
   JWT_SECRET: process.env.JWT_SECRET || 'ASDnkwlj2lkL@L1lk213j',
-  DAEMON_PORT: process.env.PORT || 3000,
-  DAEMON_HOST: process.env.HOST || 'localhost',
+  DAEMON_PORT: process.env.PORT || process.env.$PORT || 3000,
+  DAEMON_HOST: process.env.HOST || process.env.$HOST || 'localhost',
 };
