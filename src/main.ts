11dfc07d480c154879d88da17713fa25e5fde653
Astroner <zhenyanikforov@gmail.com>
2020-11-08 20:43:27 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(env.DAEMON_PORT);
   console.log(`Started on ${env.DAEMON_HOST}:${env.DAEMON_PORT}`);
 }
